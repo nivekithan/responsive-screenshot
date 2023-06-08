c55bb0edaf96fb4283f8d4832ac27ee9d2f1f3ee
Nivekithan S <nivekithan363@gmail.com>
2023-06-08 21:53:19 +0530

    [ui] Change share link to 'set access'

--- src/components/shareScreenShotLinkDialog.tsx
+++ src/components/shareScreenShotLinkDialog.tsx
@@ -11,13 +11,12 @@ import {
 } from "./ui/dialog";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
-import { useReducer, useRef } from "react";
+import { useReducer, useRef, useState } from "react";
 import { z } from "zod";
 import { X } from "lucide-react";
-import { useState } from "react";
-import { useToast } from "./ui/use-toast";
 import { PageAccessEmailModel } from "@/lib/convert";
 import { createPageAccessEmails, updatePageAccessEmails } from "@/lib/storage";
+import { toast } from "./ui/use-toast";
 
 type EmailList = Set<string>;
 type emailReducerAction =
@@ -60,14 +59,6 @@ function initializePageAccessEmailState(
   return new Set(pageAccessEmails.email);
 }
 
-function getLinkForSharing() {
-  const url = new URL(window.location.href);
-
-  url.searchParams.set("share", `true`);
-
-  return url.toString();
-}
-
 export type ShareScreenShotLinkDialogProps = {
   pageAccessEmails?: PageAccessEmailModel;
   pageId: string;
@@ -77,30 +68,29 @@ export function ShareScreenShotLinkDialog({
   pageAccessEmails,
   pageId,
 }: ShareScreenShotLinkDialogProps) {
-  const { toast } = useToast();
   const [shareWithEmails, updateEmail] = useReducer(
     shareToEmailListReducer,
     pageAccessEmails,
     initializePageAccessEmailState
   );
   const emailInputRef = useRef<HTMLInputElement | null>(null);
-
-  const [linkToShare, setLinkToShare] = useState("");
   const revalidator = useRevalidator();
-
-  const isLinkAvaliableToShare = !!linkToShare;
+  const [error, setError] = useState<string>();
+  const [open, setOpen] = useState(false);
 
   return (
-    <Dialog>
-      <DialogTrigger>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
         <Button variant="outline" size="sm">
-          Share Link
+          Set Access
         </Button>
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Share screenshot</DialogTitle>
-          <DialogDescription>Get feedback from your team</DialogDescription>
+          <DialogTitle>Access to screenshot</DialogTitle>
+          <DialogDescription>
+            Set who you can access your screenshot
+          </DialogDescription>
         </DialogHeader>
         <Form
           className="flex flex-col gap-y-2.5"
@@ -110,13 +100,21 @@ export function ShareScreenShotLinkDialog({
             const emailToAdd = z
               .string()
               .email("Please enter valid email")
-              .parse(emailInputRef.current?.value);
-            updateEmail({ type: "addNewEmail", email: emailToAdd });
-            setLinkToShare("");
-            event.currentTarget.reset();
+              .safeParse(emailInputRef.current?.value);
+
+            if (emailToAdd.success) {
+              setError("");
+              updateEmail({ type: "addNewEmail", email: emailToAdd.data });
+              event.currentTarget.reset();
+              return;
+            }
+
+            const error = emailToAdd.error.errors[0].message;
+            setError(error);
           }}
         >
           <Label htmlFor="share-screenshot">Team member email:</Label>
+
           <Input
             placeholder="team_member@company.com"
             type="email"
@@ -126,9 +124,9 @@ export function ShareScreenShotLinkDialog({
           <button type="submit" className="hidden">
             Submit
           </button>
-          {/* <p className="text-destructive text-sm font-medium">
-              {error ? error : null}
-            </p> */}
+          <p className="text-destructive text-sm font-medium">
+            {error ? error : null}
+          </p>
         </Form>
         <div className="flex flex-wrap gap-1.5 ">
           {new Array(...shareWithEmails).map((email) => {
@@ -141,7 +139,6 @@ export function ShareScreenShotLinkDialog({
                 type="button"
                 onClick={() => {
                   updateEmail({ type: "removeEmail", email });
-                  setLinkToShare("");
                 }}
               >
                 <p>{email}</p>
@@ -152,18 +149,7 @@ export function ShareScreenShotLinkDialog({
             );
           })}
         </div>
-        <DialogFooter className="flex sm:justify-between">
-          <Button
-            variant="secondary"
-            disabled={!isLinkAvaliableToShare}
-            onClick={async () => {
-              await navigator.clipboard.writeText(linkToShare);
-              toast({ description: "Link has been copied" });
-            }}
-            type="button"
-          >
-            Copy Link
-          </Button>
+        <DialogFooter className="flex">
           <Button
             onClick={async () => {
               if (pageAccessEmails) {
@@ -175,11 +161,12 @@ export function ShareScreenShotLinkDialog({
                 await createPageAccessEmails(pageId, shareWithEmails);
               }
               revalidator.revalidate();
-              toast({ description: "Link has been generated" });
-              const link = getLinkForSharing();
-              setLinkToShare(link);
+              setOpen(false);
+              navigator.clipboard.writeText(`${window.location.href}`);
+              toast({ description: "Url is copied to your clipboard" });
             }}
             type="button"
+            className="w-full"
           >
             Share
           </Button>

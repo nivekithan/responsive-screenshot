5fe1631ae6c752294fd4f9c53701f9a0239fbfe2
Nivekithan S <nivekithan363@gmail.com>
2023-06-08 21:35:16 +0530

    [ui] Show device name in floating widget

--- src/components/screenshotFloatingWidget.tsx
+++ src/components/screenshotFloatingWidget.tsx
@@ -1,9 +1,8 @@
 import { Button } from "./ui/button";
 import { Form } from "react-router-dom";
 import { z } from "zod";
-import { PageModel } from "@/lib/storage";
 import { ShareScreenShotLinkDialog } from "./shareScreenShotLinkDialog";
-import { PageAccessEmailModel } from "@/lib/convert";
+import { PageAccessEmailModel, PageModel } from "@/lib/convert";
 
 export const updateScreenshotVersionSchema = z.object({
   updateScreenshotVersion: z.literal("true"),
@@ -22,6 +21,9 @@ export function ScreenshotFloatingWidget({
     <div className="flex border px-3 py-1.5 rounded-md gap-x-3 text-accent-foreground items-center bg-background ">
       <h3 className="text-md font-semibold leading-none tracking-light ">
         {page.name}
+      </h3>
+      <h3 className="text-md leading-none font-semibold tracking-extralight ">
+        {page.screenName}
       </h3>
       <Button variant="link" size="sm" asChild>
         <a href={page.originalUrl} target="blank">

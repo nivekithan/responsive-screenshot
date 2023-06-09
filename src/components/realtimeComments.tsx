ecff5c9cb9efc19e7f2ee8eba5abf562b0327b03
Nivekithan S <nivekithan363@gmail.com>
2023-06-09 12:31:30 +0530

    [ui] Loading state for for resolving issues

--- src/components/realtimeComments.tsx
+++ src/components/realtimeComments.tsx
@@ -4,6 +4,7 @@ import { Button } from "./ui/button";
 import { Separator } from "./ui/separator";
 import { useFetcher } from "react-router-dom";
 import { z } from "zod";
+import { CommentModel } from "@/lib/convert";
 
 export type RealTimeComments = {
   pageId: string;
@@ -16,7 +17,6 @@ export const resolveIssueSchema = z.object({
 
 export function RealTimeComments({ pageId, scrollToBottom }: RealTimeComments) {
   const pageComments = useSyncPageComments(pageId);
-  const resolveIssueFetcher = useFetcher();
 
   useEffect(() => {
     scrollToBottom();
@@ -25,27 +25,32 @@ export function RealTimeComments({ pageId, scrollToBottom }: RealTimeComments) {
   return (
     <div className="flex flex-col gap-y-3">
       {pageComments.map((comment) => {
-        return (
-          <div
-            key={comment.id}
-            className="border rounded-md px-3 py-2 flex flex-col gap-y-3"
-          >
-            <h3 className="text-sm font-bold">{comment.createdByEmail}</h3>
-            <p>{comment.comment}</p>
-            <Separator />
-            <resolveIssueFetcher.Form method="post">
-              <Button
-                variant="secondary"
-                className="border hover:border-foreground/50 w-full"
-                name="resolveIssue"
-                value={comment.id}
-              >
-                Resolve
-              </Button>
-            </resolveIssueFetcher.Form>
-          </div>
-        );
+        return <RealTimeIssue comment={comment} key={comment.id} />;
       })}
+    </div>
+  );
+}
+
+function RealTimeIssue({ comment }: { comment: CommentModel }) {
+  const resolveIssueFetcher = useFetcher();
+  const isResolvingIssue = resolveIssueFetcher.state === "submitting";
+
+  return (
+    <div className="border rounded-md px-3 py-2 flex flex-col gap-y-3">
+      <h3 className="text-sm font-bold">{comment.createdByEmail}</h3>
+      <p>{comment.comment}</p>
+      <Separator />
+      <resolveIssueFetcher.Form method="post">
+        <Button
+          variant="secondary"
+          className="border hover:border-foreground/50 w-full"
+          name="resolveIssue"
+          value={comment.id}
+          disabled={isResolvingIssue}
+        >
+          {isResolvingIssue ? "Resolving..." : "Resolve"}
+        </Button>
+      </resolveIssueFetcher.Form>
     </div>
   );
 }
